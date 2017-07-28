532b190a87c26e78769f37f782aed4df75f48e6b
Jonaszko <konieczny.jonasz@gmail.com>
2017-07-28 02:27:25 +0200

    kolejny komit

--- kantor/app/controllers/Application.java
+++ kantor/app/controllers/Application.java
@@ -1,16 +1,59 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
+import java.util.List;
+import java.util.stream.Collectors;
 
-import java.util.*;
-
-import models.*;
+import models.User;
+import play.mvc.Controller;
 
 public class Application extends Controller {
 
     public static void index() {
         render();
     }
+    
+    public static void welcome() {
+    	render();
+    }
+    
+    
+ // SAVE USER
+ 	public static void saveUser( User user) {
+
+ 		List<User> users = User.findAll();
+ 		
+ 	
+ 		for(int i=0; i<users.size(); i++) {
+ 			if(users.get(i).name.equals(user.name)) {
+ 				flash.error("Istnieje użytkownik o podanym loginie, wybierz inny");
+ 				index();
+ 			}
+ 		}
+ 		
+ 		if (user.password.equals(user.passwordRepeat)) {
+ 			if (validation.hasErrors()) {
+ 				String text = validation.errors().stream().map(x -> x.getMessageKey())
+ 						.collect(Collectors.joining(", "));
+ 				flash.error(text);
+ 				index();
+
+ 			}
+ 			
+ 			
+ 			//URL url = new URL("https://coinmarketcap.com/currencies/bitcoin/#markets");
+ 			//Scanner buff = new Scanner(url.openStream());
+ 			//String url_data = buff.nextLine();
+ 			//buff.close();
+ 			
+ 			user.save();
+
+ 			welcome();
+ 			
+ 		} else {
+ 			flash.error("błędne powtórzenie hasła");
+ 			index();
+ 		}
+
+ 	}
 }
 
--- kantor/app/models/User.java
+++ kantor/app/models/User.java
@@ -1,30 +1,58 @@
 package models;
- 
-import java.util.*;
-import javax.persistence.*;
- 
-import play.db.jpa.*;
- 
-@Entity
-public class User extends Model {
- 
-    public String addressToUser;  //adres pod ktory nalezy wyslac walute koncowa
-    public String addressFromUser;  //adres z ktorego uzytkownik wyslal walute pierwotna
-    
-    public String currencyToUser; //nazwa waluty ktora uzytkownik sobie zyczy
-    public String currencyFromUser; //nazwa waluty ktora uzytkownik wyslal
-    
-    public Double amountToUser;  //wartosc waluty koncowej dla uzytkownika 
-    public Double amountFromUser; //wartosc waluty pierwotnej od uzytkownika 
 
+import javax.persistence.Entity;
+
+import play.db.jpa.Model;
+
+//@Entity
+public class User extends Model {
+
+	public String addressToUser;  //adres pod ktory nalezy wyslac walute koncowa
+	public String addressFromUser;  //adres z ktorego uzytkownik wyslal walute pierwotna
+
+	public String currencyToUser; //nazwa waluty ktora uzytkownik sobie zyczy
+	public String currencyFromUser; //nazwa waluty ktora uzytkownik wyslal
+
+	public Double amountToUser;  //wartosc waluty koncowej dla uzytkownika 
+	public Double amountFromUser; //wartosc waluty pierwotnej od uzytkownika 
+
+	public Double actualPriceCTU; //aktualna cena waluty koncowej dla uzytkownika 
+
+	public String name;
+	public String surName;
 	public String email;
-    public String password;
-    
-    public boolean isAdmin;
-    
-    public User(String email, String password) {
-        this.email = email;
-        this.password = password;
-    }
- 
+	public String password;
+	public String passwordRepeat;
+
+	public boolean isAdmin;
+
+	public User(String addressToUser, 
+			String addressFromUser, 
+			String currencyToUser, 
+			String currencyFromUser,
+			Double amountToUser,
+			Double amountFromUser,
+			Double actualPriceCTU,
+			String name,
+			String surName,
+			String email,
+			String password,
+			String passwordRepeat,
+			boolean isAdmin) 
+	{
+	this.addressToUser = addressToUser;	
+	this.addressFromUser = addressFromUser;
+	this.currencyToUser = currencyToUser;
+	this.currencyFromUser = currencyFromUser;
+	this.amountToUser = amountToUser;
+	this.amountFromUser = amountFromUser;
+	this.actualPriceCTU = actualPriceCTU;
+	this.name = name;
+	this.surName = surName;
+	this.email = email;
+	this.password = password;
+	this.passwordRepeat = passwordRepeat;
+	this.isAdmin = isAdmin;
+	}
+
 }
